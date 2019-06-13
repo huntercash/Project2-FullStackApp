14ceb994d52b25a075dcbe347422897f44206514
Hunter Cash <hunter.cash@hlflake.com>
2019-06-13 12:12:01 -0500

    progress upload

--- api/app.py
+++ api/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+import sqlalchemy 
 import pandas as pd
 import numpy as np
 import os
@@ -8,21 +10,26 @@ import os
 # Init app
 app = Flask(__name__)
 
-# Set up Database Model
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db/sqlite"
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/.sqlite"
-app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
-# ^ This is to turn off caching on static files for development.. 
-# db = SQLAlchemy(app)
 
+#################################################
+# Database Setup
+#################################################
+
+
+# Set up Database Model
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///studentloans.sqlite"
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# ^ This is to turn off caching on static files for development.. 
+db = SQLAlchemy(app)
+db.init_app(app)
 # reflect an existing database into a new model
-# Base = automap_base()
+Base = automap_base()
 # reflect the tables
-# Base.prepare(db.engine, reflect=True)
+Base.prepare(db.engine, reflect=True)
 
 # Save references to each table
-# Samples_Metadata = Base.classes.sample_metadata
-# Samples = Base.classes.samples
+Institutions = Base.classes.institutions
 
 # Set up Home index Route
 
@@ -32,6 +39,41 @@ def index():
     return render_template("index.html")
 
 # API DATA GOES HERE
+
+@app.route("/api/institutions")
+def names():
+    """Return a list of institutions names."""
+
+    # Use Pandas to perform the sql query
+    inst = db.session.query(Institutions).statement
+    df = pd.read_sql_query(inst, db.session.bind)
+
+    # Return a list of the column names (sample names)
+    return jsonify(list(df.columns)[2:])
+
+
+
+
+# @app.route("/institutions/<int_id>")
+# def institutions(int_id):
+#     """Return `'UnitID', 'institution_name', 'street', 'city', 'state', 'zipcode', 'website', 'longitude', 'latitude'`."""
+#     instns = db.session.query(Institutions).statement
+#     df = pd.read_sql_query(instns, db.session.bind)
+
+#     # Filter the data based on the sample number and
+#     # only keep rows with values above 1
+#     institution_data = df, ['UnitID', 'institution_name', 'street', 'city', 'state', 'zipcode', 'website', 'longitude', 'latitude']
+#     # Format the data to send as json
+#     data = {
+#         'unit_id': institution_data.UnitID.values.tolist(),
+#         'institution_name': institution_data.institution_name.values.tolist(),
+#         'street': institution_data.street.values.tolist(), 
+#         # "otu_ids": sample_data.otu_id.values.tolist(),
+#         # "sample_values": sample_data[sample].values.tolist(),
+#         # "otu_labels": sample_data.otu_label.tolist(),
+#     }
+#     return jsonify(data)
+
 
 
 # Run Server
