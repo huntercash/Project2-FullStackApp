4712868bec215abcd5190bba66be3aaae09bcd02
abdulib <abdulrahmon.ibrahim@yahoo.com>
2019-06-15 22:22:56 -0500

    Dashboard Setup

--- Ibrahim/js/plots.js
+++ Ibrahim/js/plots.js
@@ -6,13 +6,20 @@ var state_arr = []
 // get the data
 d3.csv("clean_data/avg_sl_debt_by_state.csv", function(error, avg_sl_debt_by_state) {
     if (error) throw error;
-        console.log(avg_sl_debt_by_state);
+        // console.log(avg_sl_debt_by_state);
 
         avg_sl_debt_by_state.forEach(function(row) {
+            row['2018'] = +row['2018'];
+            row['2019'] = +row['2019'];
+            row['% Change'] = +row['% Change'];
+            row['State'] = +row['State']
+
+
             debt_2018 = row['2018']
             debt_2019 = row['2019']
             percentage_chg = row['% Change']
             state = row['State']
+
             debt_2018_arr.push(debt_2018)
             debt_2019_arr.push(debt_2019)
             percentage_chg_arr.push(percentage_chg)
@@ -20,7 +27,60 @@ d3.csv("clean_data/avg_sl_debt_by_state.csv", function(error, avg_sl_debt_by_sta
           });
 
         //draw chart
-  })
+})
 
-//   console.log(debt_2018_arr, debt_2019_arr, percentage_chg_arr, state_arr)
+console.log(debt_2018_arr)
+
+
+
+var data = [{
+  values: [19, 26, 55],
+  labels: ['Residential', 'Non-Residential', 'Utility'],
+  type: 'pie'
+}];
+
+var layout = {
+  height: 400,
+  width: 500
+};
+
+Plotly.newPlot('plot', data, layout);
+
+
+
+
+
+var data = [
+    {
+      x: ['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'],
+      y: [1, 3, 6],
+      type: 'scatter'
+    }
+  ];
+  
+  Plotly.newPlot('plot1', data, {responsive: true});
+
+
+
+  var trace1 = {
+    type: 'bar',
+    x: [1, 2, 3, 4],
+    y: [5, 10, 2, 8],
+    marker: {
+        color: '#C8A2C8',
+        line: {
+            width: 2.5
+        }
+    }
+};
+
+var data = [ trace1 ];
+
+var layout = {
+  title: "Responsive to window's size!",
+  font: {size: 18}
+};
+
+Plotly.newPlot('plot2', data, layout, {responsive: true});
+
 
