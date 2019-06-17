61b8a7bf91ecace2f2e0de01fd88369d19a28493
abdulib <abdulrahmon.ibrahim@yahoo.com>
2019-06-17 12:09:47 -0500

    Update plots.js

--- Ibrahim/js/plots.js
+++ Ibrahim/js/plots.js
@@ -12,7 +12,7 @@ d3.csv("clean_data/avg_sl_debt_by_state.csv", function(error, avg_sl_debt_by_sta
             row['2018'] = +row['2018'];
             row['2019'] = +row['2019'];
             row['% Change'] = +row['% Change'];
-            row['State'] = +row['State']
+            row['State'] = +row['State'];
 
 
             debt_2018 = row['2018']
@@ -27,60 +27,39 @@ d3.csv("clean_data/avg_sl_debt_by_state.csv", function(error, avg_sl_debt_by_sta
           });
 
         //draw chart
+
+
 })
 
-console.log(debt_2018_arr)
+// console.log(debt_2018_arr)
 
 
+var trace1 = {
+  x: state_arr,
+  y: debt_2018_arr,
+  name: "Student Loan Debt 2018",
+  type: "bar"
+}
 
-var data = [{
-  values: [19, 26, 55],
-  labels: ['Residential', 'Non-Residential', 'Utility'],
-  type: 'pie'
-}];
+var trace2 = {
+  x: state_arr,
+  y: debt_2019_arr,
+  name: "Student Loan Debt 2019",
+  type: "bar"
+}
 
-var layout = {
-  height: 400,
-  width: 500
-};
-
-Plotly.newPlot('plot', data, layout);
-
-
-
-
-
-var data = [
-    {
-      x: ['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'],
-      y: [1, 3, 6],
-      type: 'scatter'
-    }
-  ];
-  
-  Plotly.newPlot('plot1', data, {responsive: true});
-
-
-
-  var trace1 = {
-    type: 'bar',
-    x: [1, 2, 3, 4],
-    y: [5, 10, 2, 8],
-    marker: {
-        color: '#C8A2C8',
-        line: {
-            width: 2.5
-        }
-    }
-};
-
-var data = [ trace1 ];
+var data = [trace1, trace2]
 
 var layout = {
-  title: "Responsive to window's size!",
-  font: {size: 18}
+  title: "Average Student Loan by State",
+  barmode: "group",
+  xaxis: {
+    automargin: true
+  }
 };
 
-Plotly.newPlot('plot2', data, layout, {responsive: true});
+
+Plotly.newPlot("plot", data, layout, {responsive: true});
+
 
 
