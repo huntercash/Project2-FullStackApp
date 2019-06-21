73eae32983e8586a7bc12822c398ec115ab5f89d
abdulib <abdulrahmon.ibrahim@yahoo.com>
2019-06-20 20:39:00 -0500

    Final Charts

--- Ibrahim/js/plots.js
+++ Ibrahim/js/plots.js
@@ -1,54 +1,54 @@
 
 
-// get the data
-d3.csv("clean_data/avg_sl_debt_by_state.csv", function(error, avg_sl_debt_by_state) {
-  if (error) throw error;
-  // console.log(avg_sl_debt_by_state);
+// // get the data
+// d3.csv("clean_data/avg_sl_debt_by_state.csv", function(error, avg_sl_debt_by_state) {
+//   if (error) throw error;
+//   // console.log(avg_sl_debt_by_state);
 
-  var debt_2018_arr = []
-  var debt_2019_arr = []
-  var percentage_chg_arr = []
-  var state_arr = []
+//   var debt_2018_arr = []
+//   var debt_2019_arr = []
+//   var percentage_chg_arr = []
+//   var state_arr = []
 
-  avg_sl_debt_by_state.forEach(function(row) {
-      row['2018'] = +row['2018'];
-      row['2019'] = +row['2019'];
-      row['% Change'] = +row['% Change'];
+//   avg_sl_debt_by_state.forEach(function(row) {
+//       row['2018'] = +row['2018'];
+//       row['2019'] = +row['2019'];
+//       row['% Change'] = +row['% Change'];
 
-      debt_2018_arr.push(debt_2018 = row['2018'])
-      debt_2019_arr.push(debt_2019 = row['2019'])
-      percentage_chg_arr.push(percentage_chg = row['% Change'])
-      state_arr.push(state = row['State'])
-    });
+//       debt_2018_arr.push(debt_2018 = row['2018'])
+//       debt_2019_arr.push(debt_2019 = row['2019'])
+//       percentage_chg_arr.push(percentage_chg = row['% Change'])
+//       state_arr.push(state = row['State'])
+//     });
 
-  //draw chart
-  var trace1 = {
-    x: state_arr,
-    y: debt_2018_arr,
-    name: "2018 Average",
-    type: "bar"
-  };
+//   //draw chart
+//   var trace1 = {
+//     x: state_arr,
+//     y: debt_2018_arr,
+//     name: "2018 Average",
+//     type: "bar"
+//   };
   
-  var trace2 = {
-    x: state_arr,
-    y: debt_2019_arr,
-    name: "2019 Average",
-    type: "bar"
-  };
+//   var trace2 = {
+//     x: state_arr,
+//     y: debt_2019_arr,
+//     name: "2019 Average",
+//     type: "bar"
+//   };
   
-  var data = [trace1, trace2]
+//   var data = [trace1, trace2]
   
-  var layout = {
-    title: "Average Student Loan by State",
-    barmode: "group",
-    xaxis: {
-      automargin: true
-    }
-  };
+//   var layout = {
+//     title: "Average Student Loan by State",
+//     barmode: "group",
+//     xaxis: {
+//       automargin: true
+//     }
+//   };
   
-  Plotly.newPlot("plot1", data, layout, {responsive: true});
+//   Plotly.newPlot("plot1", data, layout, {responsive: true});
 
-});
+// });
 
 
 
@@ -160,7 +160,7 @@ d3.csv("clean_data/yearly_portfolio_summary.csv", function(error, yearly_portfol
         ffelRecipients.push(row['FFEl Recipients'])
         totalRecipients.push(row['Total Recipients'])
       })
-      console.log(perkinsRecipients)
+      // console.log(perkinsRecipients)
 
   var trace1 = {
     x: years,
@@ -248,7 +248,7 @@ d3.csv("clean_data/yearly_portfolio_summary.csv", function(error, yearly_portfol
 d3.csv("clean_data/dl_status.csv", function(error, dl_status) {
   if (error) throw error;
 
-  console.log(dl_status[0])
+  // console.log(dl_status[0])
 
   var years = [], inSchs = [], graces = [], forbears = [], defers = [], cummuls = [], repays = [], others = []
 
@@ -256,7 +256,7 @@ d3.csv("clean_data/dl_status.csv", function(error, dl_status) {
 
     row['In-School'] = +row['In-School']
     row['Grace'] = +row['Grace']
-    console.log(row['Repayment'] = +row['Repayment'])
+    row['Repayment'] = +row['Repayment']
     row['Deferment'] = +row['Deferment']
     row['Forbearance'] = +row['Forbearance']
     row['Cumulative in Default'] = +row['Cumulative in Default']
@@ -338,5 +338,84 @@ d3.csv("clean_data/dl_status.csv", function(error, dl_status) {
   };
   
   Plotly.newPlot('plot5', data, layout, {responsive: true});
+
+});
+
+
+// Plot 6
+
+d3.csv("clean_data/serious_deliquency.csv", function(error, serious_deliquency) {
+  if (error) throw error;
+
+  // console.log(serious_deliquency)
+
+  var years = [], gen18 = [], gen30 = [], gen40 = [], gen50 = [], all = []
+
+  serious_deliquency.forEach(function(row){
+    // console.log(row)
+
+    gen18.push(row["18-29"] = +row["18-29"])
+    gen30.push(row["30-39"] = +row["30-39"])
+    gen40.push(row["40-49"] = +row["40-49"])
+    gen50.push(row["50+"] = +row["50+"])
+    // all.push(row["all"]  = +row["all"])
+    years.push(row['Year'])
+    
+  })
+
+  console.log(gen18)
+
+  var trace1 = {
+    x: years,
+    y: gen18,
+    mode: 'lines',
+    name: '18-29'
+  };
+  
+  var trace2 = {
+    x: years,
+    y: gen30,
+    mode: 'lines',
+    name: '30-39'
+  };
+  
+  var trace3 = {
+    x: years,
+    y: gen40,
+    mode: 'lines',
+    name: '40-49'
+  };
+
+  var trace4 = {
+    x: years,
+    y: gen50,
+    mode: 'lines',
+    name: '50+'
+  };
+
+  var trace5 = {
+    x: years,
+    y: all,
+    mode: 'lines',
+    name: 'Total'
+  };
+
+  
+  var data = [trace1, trace2, trace3, trace4, trace5];
+  
+  var layout = {
+    title: 'Loans By Age Group',
+    xaxis: {
+      title: 'Year'
+    },
+    yaxis: {
+      title: 'Amount (in billions)'
+    }
+  };
+  
+  Plotly.newPlot('plot6', data, layout);
+
+
+
 
 });
