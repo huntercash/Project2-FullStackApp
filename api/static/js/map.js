b3f19045e087b37f19d655e11990fba999d53f8b
ugochi <uakaluso@gmail.com>
2019-06-21 07:54:16 -0500

    updating index and map html

--- api/static/js/map.js
+++ api/static/js/map.js
@@ -25,14 +25,14 @@ d3.json(url, function (new_data) {
     maxZoom: 20,
     id: "mapbox.light",
     accessToken: API_KEY
-  }).addTo(myMap);
+  });
 
   var dark = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 20,
     id: "mapbox.dark",
     accessToken: API_KEY
-  });
+  }).addTo(myMap);
 
 
   // path to data from DB
@@ -109,7 +109,9 @@ d3.json(url, function (new_data) {
           });
       }
 
-      }).addTo(myMap);
+      // }).addTo(myMap);
+    });
+   
 
        // adding hover code to layer
 
